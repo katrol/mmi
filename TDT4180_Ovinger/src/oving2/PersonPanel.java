e01e5d12cda3edf81df1dd9af70b94a088e1eb21
Katrine Roland <katrine.roland@gmail.com>
2014-01-24 12:36:39 +0100

    Added some new files

---
+++ TDT4180_Ovinger/src/oving2/PersonPanel.java
@@ -0,0 +1,33 @@
+package oving2;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class PersonPanel extends JPanel {
+
+	public PersonPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
